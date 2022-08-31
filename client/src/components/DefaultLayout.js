309a9fb95c1588a4e487b76464c9764257dd4083
Viki Sangani <viki.javaj2ee@gamil.com>
2022-08-31 13:45:47 +0100

    added styles to default layout

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "../resources/css/defaultlayout.css";
 
-function DefaultLayout() {
-  return <div>DefaultLayout</div>;
+function DefaultLayout(props) {
+  return (
+    <div className="layout">
+      <div className="header">
+        <h1>Viki CV</h1>
+      </div>
+      <div className="content">{props.children}</div>
+    </div>
+  );
 }
 
 export default DefaultLayout;
